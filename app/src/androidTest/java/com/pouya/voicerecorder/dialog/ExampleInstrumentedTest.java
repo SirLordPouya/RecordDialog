c4f7ea9bb078d151b9953d4d69ded46827a1722b
Pouya <pouyaheydary@gmail.com>
2018-11-12 18:29:52 +0330

    fixed some issues

--- app/src/androidTest/java/com/pouya/voicerecorder/dialog/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/pouya/voicerecorder/dialog/ExampleInstrumentedTest.java
@@ -1,26 +1,27 @@
 package com.pouya.voicerecorder.dialog;
 
 import android.content.Context;
-import androidx.test.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
+@RunWith(AndroidJUnit4ClassRunner.class)
 public class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() throws Exception {
+    public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getContext();
 
-        assertEquals("com.deskode.recorddialog", appContext.getPackageName());
+        assertEquals("com.sirlordpouya.recorder.dialog.test", appContext.getPackageName());
     }
 }
